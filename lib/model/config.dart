1232f82934f6d8c1159e1f51a478327a38749047
Amirul Luqman <miyodeskun@gmail.com>
2022-02-12 03:35:42 +0800

    changed to https

--- lib/model/config.dart
+++ lib/model/config.dart
@@ -1,4 +1,4 @@
 class Config {
   // static const String server = "http://192.168.68.111";
-  static const String server = "http://amirulluqman.com";
+  static const String server = "https://amirulluqman.com";
 }
